ef447759fd338b68deae7edaf372948e3c5ee693
MicroIMOOC <1434246346@qq.com>
2018-05-07 17:17:44 +0800

    爬取电影列表

--- server/index.js
+++ server/index.js
@@ -1,8 +1,15 @@
-const Koa = require('koa');
-const app = new Koa();
+const Koa = require('koa')
+const views = require('koa-views')
+const { resolve } = require('path')
+
+const app = new Koa()
+
+app.use(views(resolve(__dirname, './views'), {
+    extension: 'pug'
+}))
 
 app.use(async (ctx, next) => {
-    await next();
+    await ctx.render('index', {})
 })
 
 app.listen(3000);

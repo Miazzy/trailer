a893d796d0e620b44b6f76050ab3062c80182faf
MicroIMOOC <1434246346@qq.com>
2018-05-10 21:22:38 +0800

    添加电影分类

--- server/database/init.js
+++ server/database/init.js
@@ -1,7 +1,13 @@
 const mongoose = require('mongoose')
 const db = 'mongodb://localhost/trailer'
+const glob = require('glob')
+const { resolve } = require('path')
 
 mongoose.Promise = global.Promise
+
+exports.initSchemas = () => {
+  glob.sync(resolve(__dirname, './schema', '**/*.js')).forEach(require)
+}
 
 exports.connect = () => {
   let maxConnect = 0
--- server/index.js
+++ server/index.js
@@ -1,11 +1,17 @@
 const Koa = require('koa')
 const views = require('koa-views')
+const mongoose = require('mongoose')
 const { resolve } = require('path')
-const { connect } = require('./database/init')
+const { connect, initSchemas } = require('./database/init')
 
-; (async () => {
-    await connect()
-})()
+    ; (async () => {
+        await connect()
+        initSchemas()
+
+        const Movie = mongoose.model('Movie')
+        const movies = await Movie.find({})
+        console.log(movies)
+    })()
 
 const app = new Koa()
 

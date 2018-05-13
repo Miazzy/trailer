464b9d1f518ccacd5f62158aa16c4d564451c230
MicroIMOOC <1434246346@qq.com>
2018-05-13 11:52:06 +0800

    动态加载前端路由业务组件

--- src/index.js
+++ src/index.js
@@ -1,2 +1,14 @@
-import './assets/common.sass'
+import React from 'react'
+import { render } from 'react-dom'
+import { BrowserRouter } from 'react-router-dom'
+import App from './app'
+
+const rootElement = document.getElementById('app')
+
+render(
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>,
+  rootElement
+)
 
---
+++ src/views/movie/detail.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default class Detail extends React.Component {
+  render() {
+    return (
+      <div>详情页</div>
+    )
+  }
+}
